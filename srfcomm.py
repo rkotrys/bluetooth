5ccbb222c7a525ffbec8a7ee728c6fa887aa27bf
rkotrys <robert.kotrys@gmail.com>
2022-03-24 08:54:30 +0100

    xxx

--- srfcomm.py
+++ srfcomm.py
@@ -29,7 +29,7 @@ while not end:
         client_sock.send("OK {}".format(timestamp))
         print( "Sender: {}, ts: {}, lenght: {}".format( header["host"], header['ts'], header['lenght'] ) )
         try:
-            data = client_sock.recv(header['lenght']+1).decode()
+            data = client_sock.recv(int(header['lenght'])+2).decode()
         except:
             print( "Link data error, close connection." )    
         else:
