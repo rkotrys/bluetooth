8243098dca8bc01a1ec8f9a18565b59285da8e1c
rkotrys <robert.kotrys@gmail.com>
2022-03-24 08:09:40 +0100

    xxx

--- srfcomm.py
+++ srfcomm.py
@@ -14,23 +14,25 @@ print( "RFCOMM server start at port {}".format(port) )
 while not end:
     client_sock,address = server_sock.accept()
     print( "\nAccepted connection from {}".format(address) )
-    try:
-        data = client_sock.recv(255).decode().trim()
-        print("DATA: ", data)
-        d=data.splitlines()
-        header={ d[0].trim().split(" ")[0]: d[0].trim().split(" ")[1], d[1].trim().split(" ")[0]: d[1].trim().split(" ")[1], d[2].trim().split(" ")[0]: d[2].trim().split(" ")[1]  }
-    except:
-        print( "Link header error, close connection." )
-    else:
-        print( "Sender: {}, ts: {}, lenght: {}".format( header["hostname"], header['ts'], header['lenght'] ) )
-        try:
-            data = client_sock.recv(header['lenght']+1).decode()
-        except:
-            print( "Link data error, close connection." )    
-        else:
-            print(data)    
-    finally:        
-        client_sock.close()
-        end=True
+    data = client_sock.recv(255).decode().trim()
+    print("DATA: ", data)
+#    try:
+#        data = client_sock.recv(255).decode().trim()
+#        print("DATA: ", data)
+#        d=data.splitlines()
+#        header={ d[0].trim().split(" ")[0]: d[0].trim().split(" ")[1], d[1].trim().split(" ")[0]: d[1].trim().split(" ")[1], d[2].trim().split(" ")[0]: d[2].trim().split(" ")[1]  }
+#    except:
+#        print( "Link header error, close connection." )
+#    else:
+#        print( "Sender: {}, ts: {}, lenght: {}".format( header["hostname"], header['ts'], header['lenght'] ) )
+#        try:
+#            data = client_sock.recv(header['lenght']+1).decode()
+#        except:
+#            print( "Link data error, close connection." )    
+#        else:
+#            print(data)    
+#    finally:        
+    client_sock.close()
+    end=True
 
 server_sock.close()
