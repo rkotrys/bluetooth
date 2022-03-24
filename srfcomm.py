2331444aa61f18b8c577a64d3a46c753e915296e
rkotrys <robert.kotrys@gmail.com>
2022-03-24 07:56:33 +0100

    xx

--- srfcomm.py
+++ srfcomm.py
@@ -13,12 +13,23 @@ server_sock.listen(1)
 print( "RFCOMM server start at port {}".format(port) )
 while not end:
     client_sock,address = server_sock.accept()
-    print( "Accepted connection from ",address )
-
-    data = client_sock.recv(1024).decode()
-    print( "received {}".format( data ) )
-    client_sock.close()
-    if data=='end':
+    print( "\nAccepted connection from {}".format(address) )
+    try:
+        data = client_sock.recv(255).decode().trim().splitlines()
+        header={ data[0].trim().split(" ")[0]: data[0].trim().split(" ")[1], data[1].trim().split(" ")[0]: data[1].trim().split(" ")[1], data[2].trim().split(" ")[0]: data[2].trim().split(" ")[1]  }
+    except:
+        print( "Link header error, close connection." )
+        
+    else:
+        print( "Sender: {}, ts: {}, lenght: {}".format( header["hostname"], header['ts'], header['lenght'] ) )
+        try:
+            data = client_sock.recv(header['lenght']+1).decode()
+        except:
+            print( "Link data error, close connection." )    
+        else:
+            print(data)    
+    finally:        
+        client_sock.close()
         end=True
 
 server_sock.close()
